3570de63996521aa90bf5e7ba2dc04ce5f2e2838
Elhoube_Brahim <64067001+ElhoubeBrahim@users.noreply.github.com>
2020-05-08 09:05:13 +0000

    Add files via upload

---
+++ layout/why_us.php
@@ -0,0 +1,26 @@
+<div class="why-us part">
+  <h2 class="title res-txt-blue"><?php echo $why_us['title']; ?></h2>
+  <div class="res-container res-row">
+    <div class="box res-col sm-4 res-flex">
+      <div class="image">
+        <img src="assets/images/multilanguage.svg" alt="" class="res-img">
+      </div>
+      <h6 class="res-txt-light-blue"><?php echo $why_us['subtitle-1']; ?></h6>
+      <p><?php echo $why_us['desc-1']; ?></p>
+    </div>
+    <div class="box res-col sm-4 res-flex">
+      <div class="image">
+        <img src="assets/images/learn.svg" alt="" class="res-img">
+      </div>
+      <h6 class="res-txt-light-blue"><?php echo $why_us['subtitle-2']; ?></h6>
+      <p><?php echo $why_us['desc-2']; ?></p>
+    </div>
+    <div class="box res-col sm-4 res-flex">
+      <div class="image">
+        <img src="assets/images/responsive.svg" alt="" class="res-img">
+      </div>
+      <h6 class="res-txt-light-blue"><?php echo $why_us['subtitle-3']; ?></h6>
+      <p><?php echo $why_us['desc-3']; ?></p>
+    </div>
+  </div>
+</div>

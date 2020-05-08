30ace75821cee3e2cc56c3543b4747810c5403db
Elhoube_Brahim <64067001+ElhoubeBrahim@users.noreply.github.com>
2020-05-08 09:49:57 +0000

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 var width = $('.about .member .image img').width();
 
 $('.about .member .image img').css('height', width + 'px');
+
+var colors = ['red', 'pink', 'purple', 'blue', 'green', 'yellow', 'orange', 'brown', 'grey'];
+
+colors.forEach(color => {
+  $('#colors').append('<div class="color res-col md-4"><div class="res-bg-light-' + color + '">light-' + color + '</div></div>');
+  $('#colors').append('<div class="color res-col md-4"><div class="res-bg-' + color + '">' + color + '</div></div>');
+  $('#colors').append('<div class="color res-col md-4"><div class="res-bg-dark-' + color + '">dark-' + color + '</div></div>');
+});
